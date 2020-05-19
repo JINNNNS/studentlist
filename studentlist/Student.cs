c437222f4573188dc3da7a1b06e0c2b9159989c5
JINNNNS <baekhuen@naver.com>
2020-05-19 22:27:41 +0900

    초기 커밋.

---
+++ studentlist/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace studentlist
+{
+    class Student
+    {
+        public string id;
+        public string name;
+        public int grade;
+        public string major;
+        public DateTime birthday;
+
+
+
+    }
+}
